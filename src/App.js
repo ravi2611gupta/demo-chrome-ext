158cf101f546a8af76898fb7628c9e4ec92233ac
ravi gupta <ravi2611gupta@gmail.com>
2023-09-11 13:51:11 +0530

    demo chrome extension with react done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const data = [
+  {
+    name: 'Search Engines',
+    url: ['https://www.google.com', 'https://www.bing.com']
+  }
+]
+
 function App() {
+
+  const [lists, setLists] = useState(data);
+
+  const openTabs = (url) => {
+    for(const link of url){
+      window.open(link, '_blank');
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className="App">
+      <h3>Choose your App List</h3>
+      <div className="lists">
+        {lists && lists.map((item) => <button className="button" onClick={()=> openTabs(item.url)}>{item.name}</button>)}
+      </div>     
+    </div>
     </div>
   );
 }
